deb4be2a4687fcdb4c419c3d82c29ab1982a776f
itoshin1341 <82028845+itoshin1341@users.noreply.github.com>
2022-04-06 13:51:47 +0900

    アロー関数

--- src/index.js
+++ src/index.js
@@ -67,15 +67,17 @@
 // function func1(str) {
 //   return str;
 // };
-const func1 = function (str) {
-  return str;
-};
+// const func1 = function (str) {
+//   return str;
+// };
+// console.log(func1("func1です"));
 
-console.log(func1("func1です"));
+// // アロー関数
+// const func2 = (str) => {
+//   return str;
+// };
+// console.log(func2("func2です"));
 
-// アロー関数
-const func2 = (str) => {
-  return str;
-};
+// const func3 = (num1, num2) => num1 + num2;
 
-console.log(func2("func2です"));
+// console.log(func3(10, 20));
