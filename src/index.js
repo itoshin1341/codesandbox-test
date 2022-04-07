4ef020efea574734693e640341de4cde93f0f202
Shinichiro Ito <82028845+itoshin1341@users.noreply.github.com>
2022-04-07 11:16:15 +0900

    mapやfilterを使用した配列処理

--- src/index.js
+++ src/index.js
@@ -152,18 +152,27 @@
  */
 const nameArr = ["田中", "山田", "じゃけぇ"];
 // for (let index = 0; index < nameArr.length; index++) {
-//   console.log(nameArr[index]);
+//   console.log(`${index　+　1}番目は${nameArr[index]}です。`);
 // };
 
-// const nameArr2 = nameArr.map((name)=>{
+// const nameArr2 = nameArr.map((name) => {
 //   return name;
 // });
 // console.log(nameArr2);
 
-// nameArr.map((name) => console.log(name));
+// nameArr.map((name, index) => console.log(`${index + 1}番目は${name}です。`));
 
-const numArr = [1, 2, 3, 4, 5];
-const newNumArr = numArr.filter((num) => {
-  return num % 2 === 1;
-});
-console.log(newNumArr);
+// const numArr = [1, 2, 3, 4, 5];
+// const newNumArr = numArr.filter ((num) => {
+//   return num % 2 === 1;
+// });
+// console.log(newNumArr);
+
+// const newNameArr = nameArr.map((name) => {
+//   if (name === "じゃけぇ")　{
+//     return name
+//   } else {
+//     return `${name}さん`
+//   }
+// });
+// console.log(newNameArr);
